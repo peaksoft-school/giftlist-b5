24816c30f2f919cf9db1f6c81b188654a86800ea
Dinara <dinararahatbekkyzy@gmaiil.com>
2022-07-13 20:18:36 +0600

    add bookingRepository

---
+++ src/main/java/kg/giftlist/giftlist/BookingRepository.java
@@ -0,0 +1,9 @@
+package kg.giftlist.giftlist;
+
+import kg.giftlist.giftlist.models.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+}
