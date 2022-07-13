87c9b461519779b6c6cb640f58ecc5522716e0ab
Dinara <dinararahatbekkyzy@gmaiil.com>
2022-07-13 12:26:14 +0600

    add holiday

---
+++ src/main/java/kg/giftlist/giftlist/models/Holiday.java
@@ -0,0 +1,35 @@
+package kg.giftlist.giftlist.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "holidays")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Holiday {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "holiday_gen",sequenceName = "holiday_seq", allocationSize = 1)
+    private Long id;
+
+    private String name;
+
+    private String photo;
+
+    private Date holidayDate;
+
+    private Boolean block;
+
+    private User user;
+
+    private List<Wish> wishes;
+
+}
