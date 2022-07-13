d630a5c7977c119f5145d74640a4c3b83a53a589
Dinara <dinararahatbekkyzy@gmaiil.com>
2022-07-13 12:11:16 +0600

    add entities

---
+++ src/main/java/kg/giftlist/giftlist/models/User.java
@@ -0,0 +1,48 @@
+package kg.giftlist.giftlist.models;
+
+import kg.giftlist.giftlist.enums.Role;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "user_gen",sequenceName = "user_seq", allocationSize = 1)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String password;
+
+    private Role role;
+
+    private List<User> friends = new ArrayList<>();
+
+    private List<Wish> wishes = new ArrayList<>();
+
+    private List<Holiday> gifts = new ArrayList<>();
+
+    private List<Gift> holidays = new ArrayList<>();
+
+    private Booking booking;
+
+    private Boolean block;
+
+    private UserInfo userInfo;
+
+}
