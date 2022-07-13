4e22ec46cb84321fb64eba275ab7fa172a6e61e9
Dinara <dinararahatbekkyzy@gmaiil.com>
2022-07-13 12:17:03 +0600

    add complaint

---
+++ src/main/java/kg/giftlist/giftlist/models/Complaint.java
@@ -0,0 +1,24 @@
+package kg.giftlist.giftlist.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "complaints")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Complaint {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "complaint_gen", sequenceName = "complaint_seq", allocationSize = 1)
+    private Long id;
+
+    private String text;
+
+    private User user;
+}
