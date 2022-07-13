1c725728539881250e1a61aecb2dba1c4423888c
Dinara <dinararahatbekkyzy@gmaiil.com>
2022-07-13 12:38:38 +0600

    add userInfo

---
+++ src/main/java/kg/giftlist/giftlist/models/UserInfo.java
@@ -0,0 +1,39 @@
+package kg.giftlist.giftlist.models;
+
+import kg.giftlist.giftlist.enums.ClothingSize;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "usersInfo")
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "userInfo_gen",sequenceName = "userInfo_seq", allocationSize = 1)
+    private Long id;
+
+    private String photo;
+
+    private String country;
+
+    private Date dateOfBirth;
+
+    private String phoneNumber;
+
+    private ClothingSize clothingSize;
+
+    private int shoeSize;
+
+    private String hobby;
+
+    private String importantNote;
+
+}
