22a68c085f969a8e4e45d957b10ac0677dcd9618
Dinara <dinararahatbekkyzy@gmaiil.com>
2022-07-13 12:40:40 +0600

    add wish

---
+++ src/main/java/kg/giftlist/giftlist/models/Wish.java
@@ -0,0 +1,42 @@
+package kg.giftlist.giftlist.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "wishes")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Wish {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "wish_gen",sequenceName = "wish_seq", allocationSize = 1)
+    private Long id;
+
+    private String giftName;
+
+    private String giftLink;
+
+    private String giftPhoto;
+
+    private Date date;
+
+    private String description;
+
+    private Boolean block;
+
+    private Boolean hidden;
+
+    private User user;
+
+    private User fromUser;
+
+    private Holiday holidays;
+
+}
