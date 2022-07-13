86d3c7b2b909650cafc64afbcff033e738bcac0f
Dinara <dinararahatbekkyzy@gmaiil.com>
2022-07-13 12:32:35 +0600

    add request to friends

---
+++ src/main/java/kg/giftlist/giftlist/models/RequestToFriends.java
@@ -0,0 +1,25 @@
+package kg.giftlist.giftlist.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "requestToFriends")
+@NoArgsConstructor
+@Getter
+@Setter
+public class RequestToFriends {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "requestToFriends_gen", sequenceName = "requestToFriends_seq", allocationSize = 1)
+    private Long id;
+
+    private String from;
+
+    private String to;
+
+}
