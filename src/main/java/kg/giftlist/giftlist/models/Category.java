72fbea88cecd042efd3be84a51f9d521ec8fe009
Dinara <dinararahatbekkyzy@gmaiil.com>
2022-07-13 12:15:40 +0600

    add category

---
+++ src/main/java/kg/giftlist/giftlist/models/Category.java
@@ -0,0 +1,25 @@
+package kg.giftlist.giftlist.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "categories")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "category_gen",sequenceName = "category_seq", allocationSize = 1)
+    private Long id;
+
+    private String name;
+
+    private List<SubCategory> subCategories;
+}
