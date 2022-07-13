63c4aa953a8f3a2256ec1349d3ed871fdeca9e62
Dinara <dinararahatbekkyzy@gmaiil.com>
2022-07-13 12:35:43 +0600

    add type

---
+++ src/main/java/kg/giftlist/giftlist/models/Type.java
@@ -0,0 +1,29 @@
+package kg.giftlist.giftlist.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "types")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Type {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "type_gen", sequenceName = "type_seq", allocationSize = 1)
+    private Long id;
+
+    private Wish wish;
+
+    private User user;
+
+    private Gift gift;
+
+    private Holiday holiday;
+
+}
