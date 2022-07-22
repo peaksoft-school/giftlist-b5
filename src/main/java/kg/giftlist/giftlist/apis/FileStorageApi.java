2053382550fe0409e9eb05b65f7606c06c75831e
SunflowerKun <dream26322gmail.com>
2022-07-22 11:38:03 +0600

    FileStorageApi moved to own package

--- src/main/java/kg/giftlist/giftlist/apis/FileStorageApi.java
+++ src/main/java/kg/giftlist/giftlist/apis/FileStorageApi.java
@@ -1,5 +1,6 @@
-package kg.giftlist.giftlist.config.s3;
+package kg.giftlist.giftlist.apis;
 
+import kg.giftlist.giftlist.config.s3.StorageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -7,12 +8,12 @@ import org.springframework.web.multipart.MultipartFile;
 @CrossOrigin
 @RestController
 @RequestMapping("/api/storage/")
-public class FileStorageController {
+public class FileStorageApi {
 
     private StorageService amazonClient;
 
     @Autowired
-    FileStorageController(StorageService amazonClient) {
+    FileStorageApi(StorageService amazonClient) {
         this.amazonClient = amazonClient;
     }
 
