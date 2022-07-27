f4bf1f90fa655d7db73094c68303002c3f7d52f0
Dinara <dinararahatbekkyzy@gmaiil.com>
2022-07-28 01:13:13 +0600

    add simpleResponse

---
+++ src/main/java/kg/giftlist/giftlist/dto/SimpleResponse.java
@@ -0,0 +1,16 @@
+package kg.giftlist.giftlist.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class SimpleResponse {
+
+    private String status;
+
+    private String message;
+
+}
