8f29b4503c4fa182be20ce68350e15c6b8be8b6b
Dinara <dinararahatbekkyzy@gmaiil.com>
2022-07-28 01:11:44 +0600

    add wishApi

--- src/main/java/kg/giftlist/giftlist/apis/WishApi.java
+++ src/main/java/kg/giftlist/giftlist/apis/WishApi.java
@@ -1,4 +1,55 @@
 package kg.giftlist.giftlist.apis;
 
+import kg.giftlist.giftlist.dto.SimpleResponse;
+import kg.giftlist.giftlist.dto.wish.WishRequest;
+import kg.giftlist.giftlist.dto.wish.WishResponse;
+import kg.giftlist.giftlist.services.impl.WishServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/wish")
+@PreAuthorize("hasAnyAuthority('USER')")
 public class WishApi {
+
+    private final WishServiceImpl wishService;
+
+    @PostMapping("/save")
+    public WishResponse create(@RequestBody WishRequest request){
+
+        return wishService.create(request);
+
+    }
+
+    @PutMapping("/{id}")
+    public WishResponse update(@PathVariable Long id,
+                                  @RequestBody WishRequest request) {
+        return wishService.update(id, request);
+
+    }
+
+    @GetMapping("/find/{id}")
+    public WishResponse findById(@PathVariable Long id){
+
+        return wishService.findById(id);
+
+    }
+
+    @DeleteMapping("/{id}")
+    public SimpleResponse delete(@PathVariable Long id){
+
+        return wishService.deleteById(id);
+
+    }
+
+    @GetMapping
+    public List<WishResponse> getAllWishes(){
+
+        return wishService.getAllWishes();
+
+    }
 }
