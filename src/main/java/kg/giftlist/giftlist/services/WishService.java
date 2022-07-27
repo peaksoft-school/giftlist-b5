5ea86be834e880100b1ca316fa22e1ce173ab500
Dinara <dinararahatbekkyzy@gmaiil.com>
2022-07-28 01:12:01 +0600

    add wishService

--- src/main/java/kg/giftlist/giftlist/services/WishService.java
+++ src/main/java/kg/giftlist/giftlist/services/WishService.java
@@ -1,4 +1,21 @@
 package kg.giftlist.giftlist.services;
 
+import kg.giftlist.giftlist.dto.SimpleResponse;
+import kg.giftlist.giftlist.dto.wish.WishRequest;
+import kg.giftlist.giftlist.dto.wish.WishResponse;
+
+import java.util.List;
+
 public interface WishService {
+
+    WishResponse create(WishRequest request);
+
+    WishResponse update(Long id, WishRequest wishRequest);
+
+    WishResponse findById(Long id);
+
+    SimpleResponse deleteById(Long id);
+
+    List<WishResponse> getAllWishes();
+
 }
