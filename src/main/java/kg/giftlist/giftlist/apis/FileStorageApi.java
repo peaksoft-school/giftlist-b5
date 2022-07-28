bd7c27eb305c242a4caa55e197fe2c237d5164df
SunflowerKun <dream26322gmail.com>
2022-07-28 16:00:48 +0600

    add new s3

--- src/main/java/kg/giftlist/giftlist/apis/FileStorageApi.java
+++ src/main/java/kg/giftlist/giftlist/apis/FileStorageApi.java
@@ -1,34 +1,45 @@
-//package kg.giftlist.giftlist.apis;
-//
-//import kg.giftlist.giftlist.config.s3.StorageService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.*;
-//import org.springframework.web.multipart.MultipartFile;
-//
-//import javax.annotation.security.PermitAll;
-//
-//@CrossOrigin(origins = "*", maxAge = 3600)
-//@RestController
-//@RequestMapping("api/files")
-//@RequiredArgsConstructor
-//@PermitAll
-//public class FileStorageApi {
-//
-//    private StorageService amazonClient;
-//
-//    @Autowired
-//    FileStorageApi(StorageService amazonClient) {
-//        this.amazonClient = amazonClient;
-//    }
-//
-//    @PostMapping("upload")
-//    public String uploadFile(@RequestPart(value = "file") MultipartFile file) throws Exception {
-//        return this.amazonClient.uploadFile(file);
-//    }
-//
-//    @DeleteMapping("remove")
-//    public String deleteFile(@RequestPart(value = "url") String fileUrl) {
-//        return this.amazonClient.deleteFileFromS3Bucket(fileUrl);
-//    }
-//}
+package kg.giftlist.giftlist.apis;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import kg.giftlist.giftlist.config.s3.StorageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("api/files")
+@RequiredArgsConstructor
+@CrossOrigin
+@Tag(name = "AWS S3 API", description = "Any user can upload, download or delete files")
+public class FileStorageApi {
+
+    private final StorageService storageService;
+
+    @Operation(summary = "Upload file", description = "Uploading file to the application")
+    @PostMapping("upload")
+    public ResponseEntity<String> uploadFile(@RequestParam(value = "files") MultipartFile file) {
+        return new ResponseEntity<>(storageService.uploadFile(file), HttpStatus.OK);
+    }
+
+    @Operation(summary = "Download file", description = "Downloading file from application")
+    @GetMapping("download/{fileName}")
+    public ResponseEntity<byte[]> downloadFile(@PathVariable String fileName) {
+        byte[] data = storageService.downloadFile(fileName);
+
+        return ( ResponseEntity
+                .ok().header("Content-Disposition","attachment; filename=" + fileName))
+                .body(data);
+    }
+
+    @Operation(summary = "Delete file", description = "Deleting file from application")
+    @DeleteMapping("remove/{fileName}")
+    public ResponseEntity<?> deleteFile(@PathVariable String fileName) {
+        storageService.deleteFile(fileName);
+        return new ResponseEntity<>("", HttpStatus.OK);
+    }
+
+
+}
