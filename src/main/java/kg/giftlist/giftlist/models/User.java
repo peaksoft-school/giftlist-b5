7119567e1f87eba0a800aa4045b719715cdd514b
seiitbeknarynbaev <narynbaev.seiitbek@gmail.com>
2022-08-01 14:08:29 +0600

    final refactor entities

--- src/main/java/kg/giftlist/giftlist/models/User.java
+++ src/main/java/kg/giftlist/giftlist/models/User.java
@@ -25,6 +25,7 @@ public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_gen")
     @SequenceGenerator(name = "user_gen", sequenceName = "user_seq", allocationSize = 1)
+
     private Long id;
 
     private String firstName;
