85e66216537b795e9f5b168eb9455a5c679a13d8
macbook <elmirbekalisherov24@gmail.com>
2022-08-01 14:59:48 +0600

    data- init

---
+++ src/main/resources/db/migration/V1__init-database.sql
@@ -0,0 +1,3 @@
+--
+insert into users(id, email, first_name, friend_status, is_block, last_name, password, role, booking_id, user_info_id)
+values (1,'admin@gmail.com','Admin','FRIEND',true,'Adminov','$2a$12$xEFzerKnyLVgXyBQ/ecOjuVs5rDd2KgixXHHvPSIqTN7TDnRH0Oba','ADMIN',null,null);
