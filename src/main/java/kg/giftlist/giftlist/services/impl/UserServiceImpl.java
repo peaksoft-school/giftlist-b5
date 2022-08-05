9ea5fa66c1ed182b32664911e5d53a58a928f4bc
SunflowerKun <dream26322gmail.com>
2022-08-05 18:16:14 +0600

    line changed

--- src/main/java/kg/giftlist/giftlist/services/impl/UserServiceImpl.java
+++ src/main/java/kg/giftlist/giftlist/services/impl/UserServiceImpl.java
@@ -24,7 +24,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
 import javax.annotation.PostConstruct;
 import javax.ws.rs.ForbiddenException;
 import java.io.FileInputStream;
