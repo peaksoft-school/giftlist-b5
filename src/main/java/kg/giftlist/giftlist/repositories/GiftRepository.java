9a7662a3a78a5d9bcf415478e9f3c9db6c654206
macbook <elmirbekalisherov24@gmail.com>
2022-08-11 15:13:32 +0600

    commit

--- src/main/java/kg/giftlist/giftlist/repositories/GiftRepository.java
+++ src/main/java/kg/giftlist/giftlist/repositories/GiftRepository.java
@@ -1,18 +1,9 @@
 package kg.giftlist.giftlist.repositories;
 
-import kg.giftlist.giftlist.dto.gift.GiftResponse;
-import kg.giftlist.giftlist.enums.Status;
 import kg.giftlist.giftlist.models.Gift;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface GiftRepository extends JpaRepository<Gift, Long> {
-
-    @Query("select g " +
-            "from Gift g")
-    List<GiftResponse> getGiftByBooking();
 }
--- src/main/java/kg/giftlist/giftlist/services/GiftService.java
+++ src/main/java/kg/giftlist/giftlist/services/GiftService.java
@@ -1,11 +1,5 @@
 package kg.giftlist.giftlist.services;
 
-import kg.giftlist.giftlist.dto.gift.GiftResponse;
-import kg.giftlist.giftlist.enums.Status;
-
-import java.util.List;
 
 public interface GiftService {
-
-    List<GiftResponse> findBy();
 }
