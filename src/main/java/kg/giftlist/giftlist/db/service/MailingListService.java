e4cc13ed4e9ab8cff94bf94fed7da426938a6ea3
macbook <elmirbekalisherov24@gmail.com>
2022-08-24 15:47:28 +0600

    first commit

--- src/main/java/kg/giftlist/giftlist/db/service/MailingListService.java
+++ src/main/java/kg/giftlist/giftlist/db/service/MailingListService.java
@@ -1,7 +1,12 @@
 package kg.giftlist.giftlist.db.service;
 
 import kg.giftlist.giftlist.db.models.MailingList;
+import kg.giftlist.giftlist.dto.mailing_list.SendMailingRequest;
+import org.springframework.http.ResponseEntity;
+
 
 public interface MailingListService {
     void save(MailingList mailingList);
+
+    ResponseEntity<?> send(SendMailingRequest sendMailingRequest);
 }
---
+++ src/main/java/kg/giftlist/giftlist/db/service/impl/EmailService.java
@@ -0,0 +1,29 @@
+package kg.giftlist.giftlist.db.service.impl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+
+    @Async
+    public void send(String to, String htmlMessage) {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper( mimeMessage, true, "UTF-8" );
+            helper.setFrom( "giftlist16@gmail.com" );
+            helper.setTo( to );
+            helper.setText( htmlMessage, true );
+            mailSender.send( mimeMessage );
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+}
