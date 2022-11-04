323c14342f5c465b2f9358a2e1c43807d17412f2
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-04 17:48:46 +0600

    refactor ComplaintServiceImpl

--- src/main/java/kg/giftlist/giftlist/db/service/impl/ComplaintServiceImpl.java
+++ src/main/java/kg/giftlist/giftlist/db/service/impl/ComplaintServiceImpl.java
@@ -5,28 +5,23 @@ import kg.giftlist.giftlist.db.models.Gift;
 import kg.giftlist.giftlist.db.models.Notification;
 import kg.giftlist.giftlist.db.models.User;
 import kg.giftlist.giftlist.db.models.Wish;
-
 import kg.giftlist.giftlist.db.repositories.ComplaintRepository;
 import kg.giftlist.giftlist.db.repositories.GiftRepository;
 import kg.giftlist.giftlist.db.repositories.NotificationRepository;
 import kg.giftlist.giftlist.db.repositories.UserRepository;
 import kg.giftlist.giftlist.db.repositories.WishRepository;
-
 import kg.giftlist.giftlist.dto.SimpleResponse;
 import kg.giftlist.giftlist.dto.gift.GiftResponse;
 import kg.giftlist.giftlist.dto.gift.mapper.GiftViewMapper;
 import kg.giftlist.giftlist.dto.mapper.wish.WishViewMapper;
 import kg.giftlist.giftlist.dto.wish.WishResponse;
-
 import kg.giftlist.giftlist.enums.NotificationStatus;
 import kg.giftlist.giftlist.exception.NotFoundException;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -65,13 +60,14 @@ public class ComplaintServiceImpl {
         notification.setCreatedAt(LocalDate.now());
         notification.setUser(user);
         notification.setWish(wish);
-        notification.setRecipientId(userRepo.findByRole().
-                orElseThrow(() -> new NotFoundException("User by role ADMIN not found!")).getId());
+        notification.setRecipientId(userRepo.findByRole().orElseThrow(() ->
+                new NotFoundException("User by role ADMIN not found!")).getId());
         notification.setComplaintWish(complaint);
         user.addNotification(notification);
         notificationRepository.save(notification);
-        return new SimpleResponse("Отправлено! Спасибо, что сообщили нам об этом", "Ваши отзывы помогают нам сделать сообщество GIFT LIST безопасной средой для всех");
-
+        return new SimpleResponse(
+                "Отправлено! Спасибо, что сообщили нам об этом",
+                "Ваши отзывы помогают нам сделать сообщество GIFT LIST безопасной средой для всех");
     }
 
     @Transactional
@@ -91,12 +87,14 @@ public class ComplaintServiceImpl {
         notification.setCreatedAt(LocalDate.now());
         notification.setUser(user);
         notification.setGift(gift);
-        notification.setRecipientId(userRepo.findByRole().
-                orElseThrow(() -> new NotFoundException("User by role ADMIN not found!")).getId());
+        notification.setRecipientId(userRepo.findByRole().orElseThrow(() ->
+                new NotFoundException("User by role ADMIN not found!")).getId());
         notification.setComplaintGift(complaint);
         user.addNotification(notification);
         notificationRepository.save(notification);
-        return new SimpleResponse("Отправлено!Спасибо, что сообщили нам об этом", "Ваши отзывы помогают нам сделать сообщество GIFT LIST безопасной средой для всех");
+        return new SimpleResponse(
+                "Отправлено!Спасибо, что сообщили нам об этом",
+                "Ваши отзывы помогают нам сделать сообщество GIFT LIST безопасной средой для всех");
 
     }
 
