46d5144769d7a1c2076f35be2271f3bfe7586718
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-07 14:42:07 +0600

    refactor WishEditMapper

--- src/main/java/kg/giftlist/giftlist/dto/mapper/wish/WishEditMapper.java
+++ src/main/java/kg/giftlist/giftlist/dto/mapper/wish/WishEditMapper.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Component;
 public class WishEditMapper {
 
     public Wish create(WishRequest request) {
-
         if (request == null) {
             return null;
         }
@@ -22,11 +21,11 @@ public class WishEditMapper {
     }
 
     public void update(Wish wish, WishRequest wishRequest) {
-
         wish.setWishName(wishRequest.getWishName());
         wish.setWishLink(wishRequest.getWishLink());
         wish.setWishPhoto(wishRequest.getPhoto());
         wish.setDescription(wishRequest.getDescription());
         wish.setWishDate(wishRequest.getWishDate());
     }
+
 }
