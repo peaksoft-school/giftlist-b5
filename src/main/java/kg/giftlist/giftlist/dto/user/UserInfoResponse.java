1c6bfa7e7add79fecc396d5360046a59934988ff
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-07 14:49:02 +0600

    refactor UserInfoResponse

--- src/main/java/kg/giftlist/giftlist/dto/user/UserInfoResponse.java
+++ src/main/java/kg/giftlist/giftlist/dto/user/UserInfoResponse.java
@@ -1,7 +1,6 @@
 package kg.giftlist.giftlist.dto.user;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-//import io.swagger.annotations.ApiModelProperty;
 import kg.giftlist.giftlist.enums.ClothingSize;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,9 +16,10 @@ public class UserInfoResponse {
     private String email;
     private String photo;
     private String city;
+
     @JsonFormat(pattern = "dd.MM.yyyy")
-//    @ApiModelProperty(dataType = "java.sql.Date")
     private LocalDate dateOfBirth;
+
     private String phoneNumber;
     private ClothingSize clothingSize;
     private int shoeSize;
